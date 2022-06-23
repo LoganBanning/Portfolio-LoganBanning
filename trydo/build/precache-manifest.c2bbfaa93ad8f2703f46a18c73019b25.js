881fd59155c709ef46e43933a7bf6a2ce2a04724
Logan Banning <loganboganb@gmail.com>
2022-06-23 17:22:27 -0600

    updated index.html

---
+++ trydo/build/precache-manifest.c2bbfaa93ad8f2703f46a18c73019b25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37dfd5a6d951533a5e7823fc9743d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5c9da7214f08c99b77",
+    "url": "/static/css/2.9ee80c00.chunk.css"
+  },
+  {
+    "revision": "eca9b14688c482146797",
+    "url": "/static/css/main.a61535e5.chunk.css"
+  },
+  {
+    "revision": "ba5c9da7214f08c99b77",
+    "url": "/static/js/2.988efbab.chunk.js"
+  },
+  {
+    "revision": "35c9bb817157a9e5058f",
+    "url": "/static/js/3.99f4368b.chunk.js"
+  },
+  {
+    "revision": "eca9b14688c482146797",
+    "url": "/static/js/main.5b82c476.chunk.js"
+  },
+  {
+    "revision": "c0bff5f701807383b100",
+    "url": "/static/js/runtime~main.120f2092.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
