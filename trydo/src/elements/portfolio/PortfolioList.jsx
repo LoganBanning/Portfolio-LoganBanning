752d2e393e12e79355cf40896acd0758a839d6e7
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-05-01 21:01:33 -0600

    added picture in picture project

--- trydo/src/elements/portfolio/PortfolioList.jsx
+++ trydo/src/elements/portfolio/PortfolioList.jsx
@@ -18,13 +18,13 @@ const PortfolioListContent = [
         category: 'Project',
         title: 'Student info page',
         url: 'https://cerulean-chimera-c6adbf.netlify.app/'
+    },
+    {
+        image: 'image-4',
+        category: 'Project',
+        title: 'Picture In Picture',
+        url: 'https://loganbanning.github.io/picture-in-picture/'
     }
-    // {
-    //     image: 'image-3',
-    //     category: 'Project',
-    //     title: 'Spotify Clone',
-    //     url: 'https://starfish-app-bv92g.ondigitalocean.app/'
-    // }
 ]
 
 class PortfolioList extends Component{
